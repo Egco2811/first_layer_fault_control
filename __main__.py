d2ae66e46d9bbf2782957dbbfe85cd3cadadd132
Egco <egecan2811@yandex.com>
2025-07-18 19:41:59 +0300

    send gcodes to prepare the printer

--- __main__.py
+++ __main__.py
@@ -6,4 +6,10 @@ if __name__ == '__main__':
     printer_address = "http://192.168.1.8"
     capture_image(printer_address, "snapshot.jpg")
     preprocess_image("snapshot.jpg")
+    send_gcode(printer_address, "M140 S60")
+    send_gcode(printer_address, "M190 S60")
     send_gcode(printer_address, "G28")
+    send_gcode(printer_address, "BED_MESH_CALIBRATE")
+    send_gcode(printer_address, "G90")
+    send_gcode(printer_address, "G0 X150 Y150 F6000")
+
